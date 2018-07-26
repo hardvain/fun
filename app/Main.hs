34290bc2f039dc18a28241d81640b451035d7272
Aravindh Sridharan <mail@aravindh.io>
2018-07-26 15:54:57 +0200

    GLFW Integration in progress

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,30 @@
 module Main where
 
-import Graphics.UI.GLUT
-import Lib
+import Data.List                 (intercalate)
+import Data.Maybe                (catMaybes)
+import Control.Monad             (unless, when, void)
+import qualified Graphics.Rendering.OpenGL as GL
+import qualified Graphics.UI.GLFW          as GLFW
 
 main :: IO ()
-main = do
-  (_programName, _args) <- getArgsAndInitialize
-  _window <- createWindow "Animax"
-  displayCallback $= display
-  mainLoop
+main = withWindow 1920 1280 "Fun" $ \window -> do
+  putStrLn "GLFW Started"
 
-display :: DisplayCallback
-display = do
-  clear [ColorBuffer]
-  flush
+
+withWindow :: Int -> Int -> String -> (GLFW.Window -> IO ()) -> IO ()
+withWindow width height title f = do
+    GLFW.setErrorCallback $ Just simpleErrorCallback
+    r <- GLFW.init
+    when r $ do
+        m <- GLFW.createWindow width height title Nothing Nothing
+        case m of
+          (Just win) -> do
+              GLFW.makeContextCurrent m
+              f win
+              GLFW.setErrorCallback $ Just simpleErrorCallback
+              GLFW.destroyWindow win
+          Nothing -> return ()
+        GLFW.terminate
+  where
+    simpleErrorCallback e s =
+        putStrLn $ unwords [show e, show s]
