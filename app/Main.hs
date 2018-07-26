1d1d276a5dc3508f2b7a51e89a90bbd2c0d9e448
Aravindh Sridharan <mail@aravindh.io>
2018-07-26 16:32:25 +0200

    Qualified glfw

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,13 @@ module Main where
 
 import Control.Monad (unless, when)
 import qualified Graphics.Rendering.OpenGL as GL
-import qualified Graphics.UI.GLFW as G
+import qualified Graphics.UI.GLFW as GLFW
 import System.Exit
 import System.IO
 
 -- tiny utility functions, in the same spirit as 'maybe' or 'either'
 -- makes the code a wee bit easier to read
+
 
 bool :: Bool -> a -> a -> a
 bool b falseRes trueRes = if b then trueRes else falseRes
@@ -23,34 +24,34 @@ maybe' m nothingRes f = case m of
     Just x  -> f x
     
 -- type ErrorCallback = Error -> String -> IO ()
-errorCallback :: G.ErrorCallback
+errorCallback :: GLFW.ErrorCallback
 errorCallback err description = hPutStrLn stderr description
 
-keyCallback :: G.KeyCallback
-keyCallback window key scancode action mods = when (key == G.Key'Escape && action == G.KeyState'Pressed) $
-  G.setWindowShouldClose window True
+keyCallback :: GLFW.KeyCallback
+keyCallback window key scancode action mods = when (key == GLFW.Key'Escape && action == GLFW.KeyState'Pressed) $
+  GLFW.setWindowShouldClose window True
 
 main :: IO ()
 main = do
-  G.setErrorCallback (Just errorCallback)
-  successfulInit <- G.init
+  GLFW.setErrorCallback (Just errorCallback)
+  successfulInit <- GLFW.init
   -- if init failed, we exit the program
   bool successfulInit exitFailure $ do
-      mw <- G.createWindow 1920 1280 "Simple example, haskell style" Nothing Nothing
-      maybe' mw (G.terminate >> exitFailure) $ \window -> do
-          G.makeContextCurrent mw
-          G.setKeyCallback window (Just keyCallback)
+      mw <- GLFW.createWindow 1920 1280 "Simple example, haskell style" Nothing Nothing
+      maybe' mw (GLFW.terminate >> exitFailure) $ \window -> do
+          GLFW.makeContextCurrent mw
+          GLFW.setKeyCallback window (Just keyCallback)
           mainLoop window
-          G.destroyWindow window
-          G.terminate
+          GLFW.destroyWindow window
+          GLFW.terminate
           exitSuccess
           
-mainLoop :: G.Window -> IO ()
-mainLoop w = unless' (G.windowShouldClose w) $ do
+mainLoop :: GLFW.Window -> IO ()
+mainLoop w = unless' (GLFW.windowShouldClose w) $ do
     GL.clearColor GL.$= GL.Color4 0 0.5 0.5 1
     GL.clear [GL.ColorBuffer]
-    G.swapBuffers w
-    G.pollEvents
+    GLFW.swapBuffers w
+    GLFW.pollEvents
     mainLoop w
 
 
