157fdbc7f33bcb949e2e585285f994766fe9ce21
Aravindh Sridharan <mail@aravindh.io>
2018-07-26 16:10:41 +0200

    Added clear color

--- app/Main.hs
+++ app/Main.hs
@@ -36,7 +36,7 @@ module Main where
     successfulInit <- G.init
     -- if init failed, we exit the program
     bool successfulInit exitFailure $ do
-        mw <- G.createWindow 640 480 "Simple example, haskell style" Nothing Nothing
+        mw <- G.createWindow 1920 1280 "Simple example, haskell style" Nothing Nothing
         maybe' mw (G.terminate >> exitFailure) $ \window -> do
             G.makeContextCurrent mw
             G.setKeyCallback window (Just keyCallback)
@@ -47,30 +47,8 @@ module Main where
             
   mainLoop :: G.Window -> IO ()
   mainLoop w = unless' (G.windowShouldClose w) $ do
-      (width, height) <- G.getFramebufferSize w
-      let ratio = fromIntegral width / fromIntegral height
-      
-      viewport $= (Position 0 0, Size (fromIntegral width) (fromIntegral height))
+      clearColor $= Color4 0 0 0.4 0
       clear [ColorBuffer]
-      
-      matrixMode $= Projection
-      loadIdentity
-      ortho (negate ratio) ratio (negate 1.0) 1.0 1.0 (negate 1.0)
-      matrixMode $= Modelview 0
-      
-      loadIdentity
-      -- this is bad, but keeps the logic of the original example I guess
-      Just t <- G.getTime
-      rotate ((realToFrac t) * 50) $ (Vector3 0 0 1 :: Vector3 GLdouble)
-      
-      renderPrimitive Triangles $ do
-          color  (Color3 1 0 0 :: Color3 GLdouble)
-          vertex (Vertex3 (negate 0.6) (negate 0.4) 0 :: Vertex3 GLdouble)
-          color  (Color3 0 1 0 :: Color3 GLdouble)
-          vertex (Vertex3 0.6 (negate 0.4) 0 :: Vertex3 GLdouble)
-          color  (Color3 0 0 1 :: Color3 GLdouble)
-          vertex (Vertex3 0 0.6 0 :: Vertex3 GLdouble)
-          
       G.swapBuffers w
       G.pollEvents
       mainLoop w
