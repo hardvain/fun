dde4b0c4aecfa2d126bcb22abfa48d66083d26fd
Aravindh Sridharan <mail@aravindh.io>
2018-07-26 16:01:33 +0200

    Basic working app

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,76 @@
 module Main where
 
-import Data.List                 (intercalate)
-import Data.Maybe                (catMaybes)
-import Control.Monad             (unless, when, void)
-import qualified Graphics.Rendering.OpenGL as GL
-import qualified Graphics.UI.GLFW          as GLFW
-
-main :: IO ()
-main = withWindow 1920 1280 "Fun" $ \window -> do
-  putStrLn "GLFW Started"
-
-
-withWindow :: Int -> Int -> String -> (GLFW.Window -> IO ()) -> IO ()
-withWindow width height title f = do
-    GLFW.setErrorCallback $ Just simpleErrorCallback
-    r <- GLFW.init
-    when r $ do
-        m <- GLFW.createWindow width height title Nothing Nothing
-        case m of
-          (Just win) -> do
-              GLFW.makeContextCurrent m
-              f win
-              GLFW.setErrorCallback $ Just simpleErrorCallback
-              GLFW.destroyWindow win
-          Nothing -> return ()
-        GLFW.terminate
-  where
-    simpleErrorCallback e s =
-        putStrLn $ unwords [show e, show s]
+  import Control.Monad (unless, when)
+  import Graphics.Rendering.OpenGL
+  import qualified Graphics.UI.GLFW as G
+  import System.Exit
+  import System.IO
+  
+  -- tiny utility functions, in the same spirit as 'maybe' or 'either'
+  -- makes the code a wee bit easier to read
+  
+  bool :: Bool -> a -> a -> a
+  bool b falseRes trueRes = if b then trueRes else falseRes
+  
+  unless' :: Monad m => m Bool -> m () -> m ()
+  unless' action falseAction = do
+      b <- action
+      unless b falseAction
+  
+  maybe' :: Maybe a -> b -> (a -> b) -> b
+  maybe' m nothingRes f = case m of
+      Nothing -> nothingRes
+      Just x  -> f x
+      
+  -- type ErrorCallback = Error -> String -> IO ()
+  errorCallback :: G.ErrorCallback
+  errorCallback err description = hPutStrLn stderr description
+  
+  keyCallback :: G.KeyCallback
+  keyCallback window key scancode action mods = when (key == G.Key'Escape && action == G.KeyState'Pressed) $
+    G.setWindowShouldClose window True
+  
+  main :: IO ()
+  main = do
+    G.setErrorCallback (Just errorCallback)
+    successfulInit <- G.init
+    -- if init failed, we exit the program
+    bool successfulInit exitFailure $ do
+        mw <- G.createWindow 640 480 "Simple example, haskell style" Nothing Nothing
+        maybe' mw (G.terminate >> exitFailure) $ \window -> do
+            G.makeContextCurrent mw
+            G.setKeyCallback window (Just keyCallback)
+            mainLoop window
+            G.destroyWindow window
+            G.terminate
+            exitSuccess
+            
+  mainLoop :: G.Window -> IO ()
+  mainLoop w = unless' (G.windowShouldClose w) $ do
+      (width, height) <- G.getFramebufferSize w
+      let ratio = fromIntegral width / fromIntegral height
+      
+      viewport $= (Position 0 0, Size (fromIntegral width) (fromIntegral height))
+      clear [ColorBuffer]
+      
+      matrixMode $= Projection
+      loadIdentity
+      ortho (negate ratio) ratio (negate 1.0) 1.0 1.0 (negate 1.0)
+      matrixMode $= Modelview 0
+      
+      loadIdentity
+      -- this is bad, but keeps the logic of the original example I guess
+      Just t <- G.getTime
+      rotate ((realToFrac t) * 50) $ (Vector3 0 0 1 :: Vector3 GLdouble)
+      
+      renderPrimitive Triangles $ do
+          color  (Color3 1 0 0 :: Color3 GLdouble)
+          vertex (Vertex3 (negate 0.6) (negate 0.4) 0 :: Vertex3 GLdouble)
+          color  (Color3 0 1 0 :: Color3 GLdouble)
+          vertex (Vertex3 0.6 (negate 0.4) 0 :: Vertex3 GLdouble)
+          color  (Color3 0 0 1 :: Color3 GLdouble)
+          vertex (Vertex3 0 0.6 0 :: Vertex3 GLdouble)
+          
+      G.swapBuffers w
+      G.pollEvents
+      mainLoop w
