dc89abb897cb6de25832ba0dde7910aa0bad1baf
Aravindh Sridharan <mail@aravindh.io>
2018-07-29 19:31:36 +0200

    rendered using drawable

--- app/Main.hs
+++ app/Main.hs
@@ -23,14 +23,14 @@ vertices = [
   Vertex4   0.90    0.90 0 1.0,
   Vertex4 (-0.85)   0.90 0 1.0]
 
-colorsData :: [Vertex4 Float]
+colorsData :: [Color4 Float]
 colorsData = [
-  Vertex4   0.90  0.50  0.5   0.1,  -- Triangle 1
-  Vertex4   0.35  0.14  0.5   0.2,
-  Vertex4   0.50  0.62  0.5   0.6,
-  Vertex4   0.60  0.0   0.5   0.75,  -- Triangle 2
-  Vertex4   0.20  0.20  0.5   0.04,
-  Vertex4   0.15  0.49  0.5   0.23]
+  Color4   0.90  0.50  0.5   0.1,  -- Triangle 1
+  Color4   0.35  0.14  0.5   0.2,
+  Color4   0.50  0.62  0.5   0.6,
+  Color4   0.60  0.0   0.5   0.75,  -- Triangle 2
+  Color4   0.20  0.20  0.5   0.04,
+  Color4   0.15  0.49  0.5   0.23]
 
 program = Program.createProgram "/Users/aravindhs/Aravindh/projects/haskell/fun/shaders/vertex.shader" "/Users/aravindhs/Aravindh/projects/haskell/fun/shaders/fragment.shader"
 
@@ -38,35 +38,7 @@ program = Program.createProgram "/Users/aravindhs/Aravindh/projects/haskell/fun/
 main :: IO ()
 main = do
   win <- Window.createWindow 1920 1280 "Fun"
-  mesh <- createMesh vertices colorsData
   program >>= useProgram
-  let renderHint = RenderHint GL.Triangles 0 3
-  _ <- render win mesh  renderHint
+  draw (colorsData, vertices) win
   Window.closeWindow win
   
-createMesh :: Vertices -> Vertices -> IO Mesh
-createMesh positions colorsData = do
-  vao <- createVertexArrayObject
-  positionBufferObject <- withVertexArrayObject vao $ do
-    positionBufferObject <- createArrayBuffer positions
-    let positionDescriptor = VertexAttributeDescriptor {
-      attributeLocation = 0,
-      dimension = 4
-    }
-    describeAttribute positionDescriptor
-    return positionBufferObject
-  colorBufferObject <- withVertexArrayObject vao $ do
-    colorBufferObject <- createArrayBuffer colorsData
-    let colorDescriptor = VertexAttributeDescriptor {
-      attributeLocation = 1,
-      dimension = 4
-    }
-    describeAttribute colorDescriptor
-    return colorBufferObject
-  return Mesh {
-    positionBufferObject = positionBufferObject,
-    positions = positions,
-    colors = colorsData,
-    colorBufferObject = colorBufferObject,
-    vao = vao
-  }
--- app/Renderer.hs
+++ app/Renderer.hs
@@ -5,12 +5,13 @@ import Graphics.UI.GLFW as GLFW
 import Control.Monad
 import Buffer 
 import Program
+import Shape 
 
 data Mesh = Mesh {
   positionBufferObject :: GL.BufferObject,
   positions :: [GL.Vertex4 Float],
   colorBufferObject :: GL.BufferObject,
-  colors :: [GL.Vertex4 Float],
+  colors :: [Color4 Float],
   vao :: GL.VertexArrayObject
 }
 
@@ -20,6 +21,39 @@ data RenderHint = RenderHint {
   numVertices :: Int
 }
 
+
+createMesh :: Drawable -> IO Mesh
+createMesh (colorsData, positions) = do
+  vao <- createVertexArrayObject
+  positionBufferObject <- withVertexArrayObject vao $ do
+    positionBufferObject <- createArrayBuffer positions
+    let positionDescriptor = VertexAttributeDescriptor {
+      attributeLocation = 0,
+      dimension = 4
+    }
+    describeAttribute positionDescriptor
+    return positionBufferObject
+  colorBufferObject <- withVertexArrayObject vao $ do
+    colorBufferObject <- createArrayBuffer colorsData
+    let colorDescriptor = VertexAttributeDescriptor {
+      attributeLocation = 1,
+      dimension = 4
+    }
+    describeAttribute colorDescriptor
+    return colorBufferObject
+  return Mesh {
+    positionBufferObject = positionBufferObject,
+    positions = positions,
+    colors = colorsData,
+    colorBufferObject = colorBufferObject,
+    vao = vao
+  }
+
+draw :: Drawable -> Window -> IO()
+draw drawable window = do
+  let renderHint = RenderHint GL.Triangles 0 3
+  mesh <- createMesh drawable
+  render window mesh renderHint
 
 render :: Window -> Mesh -> RenderHint -> IO ()
 render window mesh hint@(RenderHint mode startIndex numVertices) = do
