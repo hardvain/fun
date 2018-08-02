dc994fb8969053e6b5deb662e8e0a9e1fe87025a
Aravindh Sridaran <mail@aravindh.io>
2018-08-02 14:30:04 +0200

    More restructuring

--- app/Program.hs
+++ app/Program.hs
@@ -8,12 +8,12 @@ data Program = Program {
   glProgram :: GL.Program
 }
 
-
 createProgram :: String -> String -> IO Program
 createProgram vertexShaderPath fragmentShaderPath = do
-  glProgram <- loadShaders [vertexShaderInfo, fragmentShaderInfo]
+  glProgram <- loadShaders shaderInfo
   return $ Program vertexShaderInfo fragmentShaderInfo glProgram
   where 
+    shaderInfo = [vertexShaderInfo, fragmentShaderInfo]
     vertexShaderInfo = ShaderInfo GL.VertexShader (FileSource vertexShaderPath)
     fragmentShaderInfo = ShaderInfo GL.FragmentShader (FileSource fragmentShaderPath) 
 
