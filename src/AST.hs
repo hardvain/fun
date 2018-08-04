ee4859a23dae0af0f648a47f1bb65113291261e8
Aravindh Sridharan <mail@aravindh.io>
2018-08-04 15:23:29 +0200

    Revert "More code cleanup in renderer"
    This reverts commit 8ab83ec282483631cc4c86d8241978ea07ef53f0.

--- src/AST.hs
+++ src/AST.hs
@@ -17,7 +17,7 @@ data ProgramType = DefaultProg
 data UniformData  a = UniformData String a GL.UniformLocation
 
 data RenderHint = RenderHint {
-  mode :: GL.PrimitiveMode,
+  primitiveMode :: GL.PrimitiveMode,
   startIndex :: Int,
   numVertices :: Int
 }
--- src/Renderer.hs
+++ src/Renderer.hs
@@ -23,7 +23,8 @@ import SceneGraph
 
 setUniform :: Mesh -> IO ()
 setUniform mesh = do
-  let matrix = (mvpMatrix . renderable) mesh
+  let renderableObj = renderable mesh
+  let matrix = mvpMatrix renderableObj
   prog <- defaultProgram
   transformLocation <- GL.uniformLocation (glProgram prog) "transform"
   datum <- GL.newMatrix GL.ColumnMajor (toList matrix) :: IO (GL.GLmatrix GL.GLfloat)
@@ -35,18 +36,24 @@ setClearColor color = do
   GL.clear [ColorBuffer]
   return ()
 
-draw ::SceneGraph Renderable -> Window ->  Int -> Integer -> IO ()
-draw sceneGraph = drawLoop (populateMeshes sceneGraph) 
+draw ::  SceneGraph Renderable -> Window ->  Int -> Integer -> IO ()
+draw sceneGraph window frameNumber startTime  = do
+  let meshedSceneGraph = populateMeshes sceneGraph
+  drawLoop meshedSceneGraph window frameNumber startTime 
 
 drawLoop ::  SceneGraph Mesh -> Window -> Int -> Integer -> IO ()
 drawLoop sceneGraph window frameNumber startTime = do
   setClearColor $ Color4 0 0 0 1
-  _ <- pure $ fmap (setUniform >> renderMesh) sceneGraph
+  _ <- pure $ fmap (render window) sceneGraph
   GLFW.swapBuffers window
   forever $ do
     GLFW.pollEvents
     drawLoop sceneGraph window (frameNumber + 1) startTime
 
+render :: Window -> Mesh -> IO ()
+render window mesh = do
+  _ <- setUniform mesh
+  renderMesh mesh
 
 renderHint :: Mesh -> RenderHint
 renderHint mesh = RenderHint GL.Triangles 0 (numberOfVertices . drawable . renderable $ mesh)
