692477ba20386c5cf09310ddb98835ab3940a070
Aravindh Sridharan <mail@aravindh.io>
2018-08-04 15:39:01 +0200

    Removed unnecessary window param

--- src/Renderer.hs
+++ src/Renderer.hs
@@ -46,22 +46,19 @@ setClearColor color = do
   return ()
 
 draw ::  SceneGraph Renderable -> Window ->  Int -> Integer -> IO ()
-draw sceneGraph window frameNumber startTime  = do
-  let meshedSceneGraph = populateMeshes sceneGraph
-  -- mapM_ (setUniform 0) uniforms
-  drawLoop meshedSceneGraph window frameNumber startTime 
+draw sceneGraph  = drawLoop (populateMeshes sceneGraph)
 
 drawLoop ::  SceneGraph Mesh -> Window -> Int -> Integer -> IO ()
 drawLoop sceneGraph window frameNumber startTime = do
   setClearColor $ Color4 0 0 0 1
-  renderSceneGraph window sceneGraph
+  renderSceneGraph sceneGraph
   GLFW.swapBuffers window
   forever $ do
     GLFW.pollEvents
     drawLoop sceneGraph window (frameNumber + 1) startTime
 
-render :: Window -> Mesh -> IO ()
-render window mesh = do
+render ::  Mesh -> IO ()
+render  mesh = do
   let renderableObj = renderable mesh
   setUniform renderableObj
   let renderHint = RenderHint GL.Triangles 0 (numberOfVertices . drawable $ renderableObj)
@@ -71,11 +68,11 @@ renderMesh :: RenderHint -> Mesh -> IO ()
 renderMesh (RenderHint mode startIndex numVertices) mesh = withVertexArrayObject (vao mesh) $ do
   drawArrays mode (fromIntegral startIndex) (fromIntegral numVertices)
 
-renderSceneGraph :: Window -> SceneGraph  Mesh -> IO ()
-renderSceneGraph window (SceneGraph tree) = renderTree window tree
+renderSceneGraph :: SceneGraph  Mesh -> IO ()
+renderSceneGraph (SceneGraph tree) = renderTree tree
 
-renderTree :: Window -> Tree  Mesh -> IO ()
-renderTree window Empty = return ()
-renderTree window (Node mesh trees) = do
-  render window mesh
-  mapM_ (renderTree window) trees
+renderTree :: Tree  Mesh -> IO ()
+renderTree Empty = return ()
+renderTree (Node mesh trees) = do
+  render mesh
+  mapM_ (renderTree) trees
